cee2a193557487e591e945557cb524db3a6b5cad
Alberto Federico Turelli <alberto.turelli@gmail.com>
2025-11-26 20:03:07 +0100

    Updated FirstResponderKit scripts from dev branch

--- Scripts/Powershell/DeployMaintenanceScripts/update_scripts.sh
+++ Scripts/Powershell/DeployMaintenanceScripts/update_scripts.sh
@@ -10,5 +10,6 @@ cp -a $SOURCE_DIR/sp_CheckBackup/sp_CheckBackup.sql $DESTINATION_DIR/scripts_com
 cp -a $SOURCE_DIR/sp_CheckSecurity/sp_CheckSecurity.sql $DESTINATION_DIR/scripts_common/
 cp -a $SOURCE_DIR/sp_CheckTempdb/sp_CheckTempdb.sql $DESTINATION_DIR/scripts_common/
 cp -a $SOURCE_DIR/sp_whoisactive/sp_WhoIsActive.sql $DESTINATION_DIR/scripts_common/
-cp -a $SOURCE_DIR/SQL-Server-First-Responder-Kit/sp*.sql $DESTINATION_DIR/scripts/
+#cp -a $SOURCE_DIR/SQL-Server-First-Responder-Kit/sp*.sql $DESTINATION_DIR/scripts/
+cp -a $SOURCE_DIR/SQL-Server-First-Responder-Kit/Install-All-Scripts.sql $DESTINATION_DIR/scripts/
 cp -a $SOURCE_DIR/sql-server-maintenance-solution/MaintenanceSolution.sql $DESTINATION_DIR/scripts_common/
